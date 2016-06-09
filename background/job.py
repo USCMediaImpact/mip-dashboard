2b12f14b093fc629345725a256a3aa08a154ffc7
steve.yin <steve.yin@mediaimpactproject.org>
2016-06-10 02:40:17 +0800

    add datetime import

--- background/job.py
+++ background/job.py
@@ -2,10 +2,11 @@
 # -*- coding: utf-8 -*-
 import logging
 import webapp2
+from datetime import datetime
 
 class DailyTaskHandler(webapp2.RequestHandler):
     def get(self):
-        logging.debug('corn job is running at' + unicode(datetime.datetime.now()))
+        logging.debug('corn job is running at' + unicode(datetime.now()))
         self.response.out.write('ok')
 
 app = webapp2.WSGIApplication([
