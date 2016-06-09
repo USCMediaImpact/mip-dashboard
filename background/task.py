ddb69d0d64d0ea4b4ea5ba66e9a0b0b686cb67c4
steve.yin <steve.yin@mediaimpactproject.org>
2016-06-10 02:13:03 +0800

    fix background task error

--- background/task.py
+++ background/task.py
@@ -3,12 +3,12 @@ import webapp2
 
 class ETLWorker(webapp2.RequestHandler):
   def get(self):
-    self.response.write('ok');
+    self.response.out.write('ok')
 
 ​class DailyTaskHandler(webapp2.RequestHandler):
     def get(self):
         logging.debug('corn job is running at' + unicode(datetime.datetime.now()))
-        self.response.write('ok');
+        self.response.out.write('ok')
 ​
 app = webapp2.WSGIApplication([
     ('/etl/daily', DailyTaskHandler),
