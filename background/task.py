f0aa5714423417ce504d1be8cd3248e4391db530
steve.yin <steve.yin@mediaimpactproject.org>
2016-06-10 02:19:03 +0800

    background debug

--- background/task.py
+++ background/task.py
@@ -1,16 +1,10 @@
 import logging
 import webapp2
 
-class ETLWorker(webapp2.RequestHandler):
-  def get(self):
-    self.response.out.write('ok')
-
 ​class DailyTaskHandler(webapp2.RequestHandler):
     def get(self):
         logging.debug('corn job is running at' + unicode(datetime.datetime.now()))
-        self.response.out.write('ok')
 ​
 app = webapp2.WSGIApplication([
-    ('/etl/daily', DailyTaskHandler),
-    ('/_ah/start', ETLWorker)
+    ('/etl/daily', DailyTaskHandler)
 ], debug=True)
