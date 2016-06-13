c603bc62a1cea09004f50bf87d138e0430ef8f58
steve.yin <steve.yin@mediaimpactproject.org>
2016-06-14 00:22:16 +0800

    daily corn job

--- background/job.py
+++ background/job.py
@@ -6,17 +6,27 @@ from datetime import datetime
 import analyticsClient
 import bigQueryClient
 import mySqlCleint
+from query import hive
+from query import sql
+
 
 class DailyTaskHandler(webapp2.RequestHandler):
     def get(self):
         logging.debug('weekly corn job is running at' + unicode(datetime.now()))
         profile_id = {'KPCC': '104512889'}
-        today = date.today().isoformat()
-        ga_data = get_ga_result('104512889', today, today, 'ga:users', 'ga:date')
-        print ga_data
-        # insert_mysql((
-        # 	"insert into "
-        # 	), ga_data)
+        yesterday = (date.today() - timedelta(1)).strftime('%Y-%m-%d')
+        ga_data = get_ga_result('104512889', yesterday, yesterday, 'ga:users', 'ga:date')
+        
+        logging.debug('KPCC ga user: %s' % (ga_data,))
+        
+        bg_data = get_bg_result(hive.data_quanlity.format(
+        	min_date=yesterday, 
+        	max_date=yesterday))
+
+        logging.debug('KPCC bigquery result : %s' % (bg_data,))
+
+        insert_mysql(sql.data_quanlity_daily, ga_data + bg_data)
+        
         self.response.out.write('ok')
 
 app = webapp2.WSGIApplication([
