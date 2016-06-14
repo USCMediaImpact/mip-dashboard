109975f51de4a89d8aa6c1b29e554f3d106ae09a
steve.yin <steve.yin@mediaimpactproject.org>
2016-06-14 16:57:28 +0800

    fix outer indentation level

--- background/job.py
+++ background/job.py
@@ -5,7 +5,7 @@ import webapp2
 from datetime import datetime
 import analyticsClient
 import bigQueryClient
-import mySqlCleint
+import mySqlClient
 from query import hive
 from query import sql
 
@@ -15,17 +15,21 @@ class DailyTaskHandler(webapp2.RequestHandler):
         logging.debug('weekly corn job is running at' + unicode(datetime.now()))
         profile_id = {'KPCC': '104512889'}
         yesterday = (date.today() - timedelta(1)).strftime('%Y-%m-%d')
-        ga_data = get_ga_result('104512889', yesterday, yesterday, 'ga:users', 'ga:date')
+        ga_data = analyticsClient.get_ga_result('104512889', 
+        	yesterday, 
+        	yesterday, 
+        	'ga:users', 
+        	'ga:date')
         
         logging.debug('KPCC ga user: %s' % (ga_data,))
         
-        bg_data = get_bg_result(hive.data_quanlity.format(
+        bg_data = bigQueryClient.get_bg_result(hive.data_quanlity.format(
         	min_date=yesterday, 
         	max_date=yesterday))
 
         logging.debug('KPCC bigquery result : %s' % (bg_data,))
 
-        insert_mysql(sql.data_quanlity_daily, (yesterday, '') + ga_data + bg_data)
+        mySqlClient.insert_mysql(sql.data_quanlity_daily, (yesterday, '') + ga_data + bg_data)
 
         self.response.out.write('ok')
 
--- background/mySqlClient.py
+++ background/mySqlClient.py
@@ -4,26 +4,26 @@ import MySQLdb
 
 """
 	sql = ("INSERT INTO employees "
-           "(first_name, last_name, hire_date, gender, birth_date) "
-           "VALUES (%s, %s, %s, %s, %s)")
-    data = [('Geert', 'Vanderkelen', tomorrow, 'M', date(1977, 6, 14)), ('Geert', 'Vanderkelen', tomorrow, 'M', date(1977, 6, 14))]
+		   "(first_name, last_name, hire_date, gender, birth_date) "
+		   "VALUES (%s, %s, %s, %s, %s)")
+	data = [('Geert', 'Vanderkelen', tomorrow, 'M', date(1977, 6, 14)), ('Geert', 'Vanderkelen', tomorrow, 'M', date(1977, 6, 14))]
 """
 def insert_mysql(sql, data):
 	env = os.getenv('SERVER_SOFTWARE')
-  if (env and env.startswith('Google App Engine/')):
-    # Connecting from App Engine
-    db = MySQLdb.connect(
-      unix_socket='/cloudsql/mip-dashboard:test-mip-dashboard',
-      database='media_impact',
-      user='root')
-  else:
-    # Connecting from an external network.
-    # Make sure your network is whitelisted
-    db = MySQLdb.connect(
-      host='127.0.0.1',
-      database='media_impact',
-      port=3306,
-      user='root')
+  	if (env and env.startswith('Google App Engine/')):
+		# Connecting from App Engine
+		db = MySQLdb.connect(
+			unix_socket='/cloudsql/mip-dashboard:test-mip-dashboard',
+			database='media_impact',
+			user='root')
+	else:
+		# Connecting from an external network.
+		# Make sure your network is whitelisted
+		db = MySQLdb.connect(
+			host='127.0.0.1',
+			database='media_impact',
+			port=3306,
+			user='root')
 
 	cursor = db.cursor()
 	for row in data:
