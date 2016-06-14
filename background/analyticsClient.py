1e6978dde03d2969f088e5c298c3b17c096fb3d7
steve.yin <steve.yin@mediaimpactproject.org>
2016-06-14 13:37:50 +0800

    fix google api client authorize

--- background/analyticsClient.py
+++ background/analyticsClient.py
@@ -1,29 +1,26 @@
 #!/usr/bin/env python
 
 import argparse
-from googleapiclient.discovery import build
-from oauth2client.client import SignedJwtAssertionCredentials
-import httplib2
-from oauth2client import client
-from oauth2client import file
-from oauth2client import tools
-from OpenSSL import crypto
+import sys
+
+from apiclient.discovery import build
+from oauth2client.service_account import ServiceAccountCredentials
+from httplib2 import Http
 
 service_account_email = 'account-1@methodical-bee-111016.iam.gserviceaccount.com'
 scope = ['https://www.googleapis.com/auth/analytics.readonly']
-key_file_location = '3024963272a5.p12'
+p12_file_location = 'mip-analytics.p12'
 api_name = 'analytics'
 api_version = 'v3'
 
 def get_service():
-	key_file = open(key_file_location, 'rb')
-	key = key_file.read()
-	key_file.close()
+	credentials = ServiceAccountCredentials.from_p12_keyfile(
+    	service_account_email, 
+    	p12_file_location, 
+    	scope)
 
-	credentials = SignedJwtAssertionCredentials(service_account_email, key, scope=scope)
-	
-	http = credentials.authorize(httplib2.Http())
-	
+	http_auth = credentials.authorize(Http())
+
 	# Build the service object.
 	return build(api_name, api_version, http=http)
 
@@ -34,9 +31,15 @@ def get_ga_result(profile_id, start_date, end_date, metrics, dimensions):
 		start_date=start_date,
 		end_date=end_date,
 		metrics=metrics,
-		dimensions=dimensions).execute()
+		dimensions=dimensions,
+		#sort='-ga:visits',
+      	#filters='ga:medium==organic',
+      	#start_index='1',
+      	#max_results='25'
+      ).execute()
 
-   	for row in data :
-   		result.push(data.get('rows'))
-   		
+    if data.get('rows', []):
+	   	for row in data.get('rows') :
+	   		result.push(row)
+
    	return result
