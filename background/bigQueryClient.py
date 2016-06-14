3443ebfa4f7bad861dd539fbfbb2e421727a0a51
steve.yin <steve.yin@mediaimpactproject.org>
2016-06-14 16:47:11 +0800

    fix outer indentation level

---
+++ background/bigQueryClient.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import argparse
+import logging
+from apiclient.discovery import build
+from googleapiclient.errors import HttpError
+from oauth2client.client import GoogleCredentials
+
+PROJECT_ID = 'mip-dashboard'
+
+def get_services():
+	credentials = GoogleCredentials.get_application_default()
+	return build('bigquery', 'v2', credentials=credentials)
+
+def get_bq_result(sql):
+	result = []
+	query_request = get_services().jobs()
+	query_data = {'query': sql}
+	query_response = query_request.query(
+		projectId=PROJECT_ID,
+		body=query_data
+	).execute()
+	response = query.get('rows', [])
+	if response :
+		for row in response :
+			dataRow = ()
+			for field in row['f'] :
+				dataRow += (field['v'],)
+			result.append(dataRow)
+	return result
