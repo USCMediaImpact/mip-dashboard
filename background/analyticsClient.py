45269b8991914be6fed534e9c2b63394b2a24042
steve.yin <steve.yin@mediaimpactproject.org>
2016-06-14 19:19:04 +0800

    debug autho2

--- background/analyticsClient.py
+++ background/analyticsClient.py
@@ -9,15 +9,16 @@ from httplib2 import Http
 
 service_account_email = 'account-1@methodical-bee-111016.iam.gserviceaccount.com'
 scope = ['https://www.googleapis.com/auth/analytics.readonly']
-p12_file_location = 'mip-analytics.pem'
+p12_file_location = 'mip-analytics.p12'
 api_name = 'analytics'
 api_version = 'v3'
 
 def get_service():
 	credentials = ServiceAccountCredentials.from_p12_keyfile(
-    	service_account_email, 
-    	p12_file_location, 
-    	scope)
+		service_account_email, 
+		p12_file_location, 
+		'nocerts',
+		scope)
 
 	http_auth = credentials.authorize(Http())
 
@@ -25,22 +26,22 @@ def get_service():
 	return build(api_name, api_version, http=http)
 
 def get_ga_result(profile_id, start_date, end_date, metrics, dimensions):
-   	query = get_service().data().ga().get(
+	query = get_service().data().ga().get(
 		ids='ga:' + profile_id,
 		start_date=start_date,
 		end_date=end_date,
 		metrics=metrics,
 		dimensions=dimensions,
 		#sort='-ga:visits',
-      	#filters='ga:medium==organic',
-      	#start_index='1',
-      	#max_results='25'
+		#filters='ga:medium==organic',
+		#start_index='1',
+		#max_results='25'
 		).execute()
 
-   	result = []
-   	response = query.get('rows', [])
-   	if response:
-   		for row in response:
-   			result.push(row)
+	result = []
+	response = query.get('rows', [])
+	if response:
+		for row in response:
+			result.push(row)
 
-   	return result
+	return result
