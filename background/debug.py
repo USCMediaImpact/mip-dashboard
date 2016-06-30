5382460bc5a9fcfec35d1386b3e4a93a3888e577
steve.yin <steve.yin@mediaimpactproject.org>
2016-06-30 21:46:50 +0800

    debug

--- background/debug.py
+++ background/debug.py
@@ -2,8 +2,14 @@ import logging
 import logging_config
 import job
 
-job.run('2016-06-25', '2016-06-25', 'weekly')
+from datetime import datetime
+from datetime import date
+from datetime import timedelta
+import calendar
+#---------single debug ---------#
+#job.run('2016-06-25', '2016-06-25', 'weekly')
 
+#---------big query debug ---------#
 # import bigQueryClient
 
 # sql = ('''SELECT '''
@@ -43,3 +49,51 @@ job.run('2016-06-25', '2016-06-25', 'weekly')
 # datatable = 'donors'
 
 # bigQueryClient.insert_from_query(sql, pid, dataset, datatable)
+
+#---------histry task ---------#
+# run history task
+min_date = date(2016, 4, 17)
+max_date = date(2016, 6, 30)
+#every day history
+# day_count = (max_date - min_date).days
+# for single_date in (min_date + timedelta(n) for n in range(day_count)):
+# 	try:
+# 		logging.info('run daily history %s', single_date.strftime('%Y-%m-%d'))
+# 		job.run(single_date.strftime('%Y-%m-%d'), single_date.strftime('%Y-%m-%d'), 'daily')
+# 		logging.info('success')
+# 	except:
+# 		logging.error('faild')
+
+#every week history
+min_week = min_date - timedelta(min_date.weekday() + 1)
+max_week = min_week + timedelta(6)
+while True:
+	try:
+		logging.info('run weekly history from %s to %s', min_week.strftime('%Y-%m-%d'), max_week.strftime('%Y-%m-%d'))
+		job.run(min_week.strftime('%Y-%m-%d'), max_week.strftime('%Y-%m-%d'), 'weekly')
+		logging.info('success')
+	except:
+		logging.error('faild')
+
+	if max_week > max_date :
+		break
+	min_week += timedelta(7)
+	max_week += timedelta(7)
+
+#every month history
+# min_month = date(min_date.year, min_date.month, 1)
+# max_month = add_months(min_month, 1) - timedelta(1)
+# while True:
+# 	try:
+# 		logging.info('run monthly history from %s to %s', min_month.strftime('%Y-%m-%d'), max_month.strftime('%Y-%m-%d'))
+# 		job.run(min_month.strftime('%Y-%m-%d'), max_month.strftime('%Y-%m-%d'), 'monthly')
+# 		logging.info('success')
+# 	except:
+# 		logging.error('faild')
+
+# 	if max_month > max_date :
+# 		break
+# 	min_month = add_months(min_month, 1)
+# 	max_month = add_months(min_month, 1) - timedelta(1)
+
+logging.info('run history job finished')
