971870d9423e473f1c37ec890081cd4168e6e743
steve.yin <steve.yin@mediaimpactproject.org>
2016-07-19 23:33:28 +0800

    change debug to error

--- background/logging_config.py
+++ background/logging_config.py
@@ -5,7 +5,7 @@ root = logging.getLogger()
 root.setLevel(logging.DEBUG)
 
 ch = logging.StreamHandler(sys.stdout)
-ch.setLevel(logging.DEBUG)
+ch.setLevel(logging.ERROR)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 ch.setFormatter(formatter)
 root.addHandler(ch)
