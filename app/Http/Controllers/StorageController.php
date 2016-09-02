322e7d72fec762ecee40c235c34d52fb127902d5
steve.yin <steve.yin@mediaimpactproject.org>
2016-09-02 22:13:44 +0800

    show logo file for super admin

--- app/Http/Controllers/StorageController.php
+++ app/Http/Controllers/StorageController.php
@@ -4,17 +4,19 @@ namespace App\Http\Controllers;
 
 use App\Models\Client;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class StorageController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('guest');
+        
     }
 
     public function showLogo(Request $request, $client_code)
     {
         $client = Client::where('code', $client_code)->first();
+        dd($client->logo);
         if ($client) {
             return response()->make(file_get_contents($client->logo), 200, [
                 'Content-Type' => 'image/png'
