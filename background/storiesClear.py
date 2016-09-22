b64616e5dd6238c0e154cd0edfc0a01c662e0763
steve.yin <steve.yin@mediaimpactproject.org>
2016-09-22 17:29:40 +0800

    add logging

--- background/storiesClear.py
+++ background/storiesClear.py
@@ -1,7 +1,9 @@
 import mySqlClient
 from query import mysql
+import logging
 
 def reduce_stories(code) :
+	logging.info('begin run %s stories clearup')
 	db = mySqlClient.get_db()
 	cursor = db.cursor()
 	cursor.execute('SET NAMES utf8;')
@@ -9,13 +11,14 @@ def reduce_stories(code) :
 	cursor.execute('SET character_set_connection=utf8;')
 	query = cursor.execute('select date from %s_data_stories_weekly' % (code,))
 	all_date = cursor.fetchmany(query)
-	print all_date
 	for date in all_date:
-		print date[0]
+		logging.info('clear up %s date' % date[0])
 		try:
 			sql = "INSERT INTO SCPR_data_stories_weekly_top100 SELECT * FROM SCPR_data_stories_weekly WHERE date = '%s' ORDER BY Pageviews DESC LIMIT 100" % date
 			cursor.execute(sql)
 			db.commit()
 		except:
+			logging.error('Failed clear up %s date %s' % (code, date[0]))
 			db.rollback()
 	db.close()
+	logging.info('run %s stories clearup completed')
