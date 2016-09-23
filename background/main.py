ba27b8165f1428b15dd649dda678a00994831a43
steve.yin <steve.yin@mediaimpactproject.org>
2016-09-24 03:38:43 +0800

    remove hisotry dialy and monthly job

--- background/main.py
+++ background/main.py
@@ -10,6 +10,13 @@ import calendar
 import job
 from dateutil.parser import parse
 
+def add_months(sourcedate, months):
+	month = sourcedate.month - 1 + months
+	year = int(sourcedate.year + month / 12 )
+	month = month % 12 + 1
+	day = min(sourcedate.day,calendar.monthrange(year,month)[1])
+	return date(year,month,day)
+
 class DailyTaskHandler(webapp2.RequestHandler):
 	def get(self):
 		yesterday = (date.today() - timedelta(1)).strftime('%Y-%m-%d')
@@ -59,14 +66,14 @@ class HistoryTaskHandler(webapp2.RequestHandler):
 		max_date = parse(self.request.get('max_date'))
 
 		#every day history
-		day_count = (max_date - min_date).days
-		for single_date in (min_date + timedelta(n) for n in range(day_count)):
-			try:
-				logging.info('run daily history %s', single_date.strftime('%Y-%m-%d'))
-				job.run(single_date.strftime('%Y-%m-%d'), single_date.strftime('%Y-%m-%d'), 'daily')
-				logging.info('success')
-			except:
-				logging.error('faild')
+		# day_count = (max_date - min_date).days
+		# for single_date in (min_date + timedelta(n) for n in range(day_count)):
+		# 	try:
+		# 		logging.info('run daily history %s', single_date.strftime('%Y-%m-%d'))
+		# 		job.run(single_date.strftime('%Y-%m-%d'), single_date.strftime('%Y-%m-%d'), 'daily')
+		# 		logging.info('success')
+		# 	except:
+		# 		logging.error('faild')
 
 		#every week history
 		min_week = min_date - timedelta(min_date.weekday() + 1)
@@ -85,20 +92,20 @@ class HistoryTaskHandler(webapp2.RequestHandler):
 			max_week += timedelta(7)
 
 		#every month history
-		min_month = date(min_date.year, min_date.month, 1)
-		max_month = add_months(min_month, 1) - timedelta(1)
-		while True:
-			try:
-				logging.info('run monthly history from %s to %s', min_month.strftime('%Y-%m-%d'), max_month.strftime('%Y-%m-%d'))
-				job.run(min_month.strftime('%Y-%m-%d'), max_month.strftime('%Y-%m-%d'), 'monthly')
-				logging.info('success')
-			except:
-				logging.error('faild')
+		# min_month = date(min_date.year, min_date.month, 1)
+		# max_month = add_months(min_month, 1) - timedelta(1)
+		# while True:
+		# 	try:
+		# 		logging.info('run monthly history from %s to %s', min_month.strftime('%Y-%m-%d'), max_month.strftime('%Y-%m-%d'))
+		# 		job.run(min_month.strftime('%Y-%m-%d'), max_month.strftime('%Y-%m-%d'), 'monthly')
+		# 		logging.info('success')
+		# 	except:
+		# 		logging.error('faild')
 
-			if max_month > max_date :
-				break
-			min_month = add_months(min_month, 1)
-			max_month = add_months(min_month, 1) - timedelta(1)
+		# 	if max_month > max_date :
+		# 		break
+		# 	min_month = add_months(min_month, 1)
+		# 	max_month = add_months(min_month, 1) - timedelta(1)
 
 		logging.info('run history job finished')
 		self.response.out.write('ok')
