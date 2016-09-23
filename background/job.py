8c197acea1564f1b1cc634cfccfffa979b59a794
steve.yin <steve.yin@mediaimpactproject.org>
2016-09-23 21:43:21 +0800

    fix table name

--- background/job.py
+++ background/job.py
@@ -228,7 +228,7 @@ def _run_data_quality(client_id, code, setting, min_date, max_date, dimension):
 
 	mySqlClient.insert_mysql(sql, [sql_data])
 
-def _run_data_newsletter(file_name, code, dimension='Weekly'):
+def _run_data_newsletter(file_name, code, dimension='weekly'):
 	logging.debug('run newsletter csv import for file: %s' % (file_name,))
 	import os, csv
 	file_obj = StringIO.StringIO()
